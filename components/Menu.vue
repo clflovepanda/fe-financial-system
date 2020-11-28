916e0f4b9995c4e3d6976c25aa04200273dd015b
yinwensong <yinwensong@yinwensongdeMacBook-Air.local>
2020-11-28 23:01:09 +0800

    新增项目列表

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,23 +1,11 @@
 <template>
   <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-    <el-submenu index="1">
-      <template slot="title">
+     <nuxt-link to="/projectList">
+      <el-menu-item index="1">
         <i class="el-icon-location"></i>
-        <span>项目列表</span>
-      </template>
-      <el-menu-item-group>
-        <template slot="title">分组一</template>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-      <el-submenu index="1-4">
-        <template slot="title">选项4</template>
-        <el-menu-item index="1-4-1">选项1</el-menu-item>
-      </el-submenu>
-    </el-submenu>
+        <span slot="title">项目列表</span>
+      </el-menu-item>
+    </nuxt-link>
     <el-menu-item index="2">
       <i class="el-icon-menu"></i>
       <span slot="title">导航二</span>
---
+++ pages/projectList.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-container class="container">
+    <el-header class="headerContent">
+      <HeaderBar />
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <Menu />
+      </el-aside>
+      <el-main>
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item class="breadcrumb">位置</el-breadcrumb-item>
+          <el-breadcrumb-item class="breadcrumb">项目</el-breadcrumb-item>
+          <el-breadcrumb-item class="breadcrumb" :to="{ path: '/projectList' }" >项目列表</el-breadcrumb-item>
+        </el-breadcrumb>
+        项目列表
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+
+
+export default {
+  methods: {
+   
+  }
+};
+
+</script>
