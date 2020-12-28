581019737cfc79b9bb468b57a30b6c2537c90208
he <hpz18301602774@163.com>
2020-12-28 22:49:31 +0800

    押金管理

---
+++ pages/depositmanage.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item class="breadcrumb">位置</el-breadcrumb-item>
+      <el-breadcrumb-item class="breadcrumb">财务</el-breadcrumb-item>
+      <el-breadcrumb-item class="breadcrumb" :to="{ path: '/description' }"
+        >押金管理</el-breadcrumb-item
+      >
+    </el-breadcrumb>
+    <el-divider></el-divider>
+    <Deposit />
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Deposit from "~/components/projectListPage/Deposit.vue";
+
+export default {
+
+  methods: {
+    
+  },
+  async asyncData(ctx) {
+    
+  }
+};
+
+</script>
